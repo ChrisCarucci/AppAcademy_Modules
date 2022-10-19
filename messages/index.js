adf149c9b0a4f532bae5b8c290111ce71ee1e89a
ChrisCarucci <Chris.w.carucci@gmail.com>
2022-10-19 13:23:41 -0230

    Import-export module practice

---
+++ messages/index.js
@@ -0,0 +1,8 @@
+const {sayHelloTo} = require("../send-messages/say-hello-to")
+const {giveMessageToMrsPotato} = require("../send-messages/give-message-to-mrs-potato")
+const msgs = require("./index");
+
+
+const message1 = "You are looking very spudly today!";
+const message2 = "There's a good deal on mattresses tomorrow.";
+const message3 = "Do you like to do things in unnecessarily complicated ways?";
